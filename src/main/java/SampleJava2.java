ea2025320797f5ac067cbe6fc6e1751b697936e2
labanya <labanyahota15@gmail.com>
2025-09-18 12:57:50 +0530

    This is a commit from Asian Timezone guy

---
+++ src/main/java/SampleJava2.java
@@ -0,0 +1,12 @@
+public class SampleJava2 {
+    public static void main(String[] args) {
+        System.out.println("Main class");
+        SampleJava2 s = new SampleJava2();
+        s.Test1();
+    }
+
+    public void Test1()
+    {
+        System.out.println("Hi, I am testing this!!");
+    }
+}
